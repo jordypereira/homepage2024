2222c2347fe92ba330b8df8247746a2a27c3d574
Jordy <jordy.pereira@hotmail.com>
2024-10-08 03:00:28 +0200

    feat: add culture page and integrate Notion films data; update global styles and navbar

---
+++ pages/culture.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+const { films } = await useNotion()
+</script>
+
+<template>
+  <article class="md:max-w-4xl mx-6 md:mx-auto md:px-6">
+    <div>
+      <h2>Culture Lists</h2>
+      <div>
+        <h3>Films</h3>
+        <ul>
+          <li v-for="film in films" :key="film.id">
+            <h4>{{ film.name }}</h4>
+            <p>Year: {{ film.year }}</p>
+            <p>Rating: {{ film.rating }}</p>
+            <p>Tags: {{ film.tags.join(', ') }}</p>
+            <p>Analysis: {{ film.analysis }}</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </article>
+</template>
+
+<style scoped>
+li {
+  margin-bottom: 1rem;
+  list-style: none;
+}
+
+</style>
